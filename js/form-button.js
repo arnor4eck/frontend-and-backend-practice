064c03f84910cb65fd85280bd803775a04ce6920
arnor4eck <KivalinNik@mail.ru>
2025-10-06 21:50:24 +0300

    added js

--- js/form-button.js
+++ js/form-button.js
@@ -1,3 +1,28 @@
 function submitForm() {
-    var a = document.getElementById("feedbackForm")
+    var form = document.getElementById("feedbackForm");
+    const formData = new FormData(form);
+    
+    if (!form.checkValidity()) {
+        form.reportValidity();
+        return;
+    }
+
+    const data = {
+        name: formData.get('name'),
+        phone: formData.get('phone'),
+        email: formData.get('email'),
+        category: formData.get('category'),
+        message: formData.get('message')
+    };
+    contactModal.close();
+    form.reset();
+
+    alert("Данные приняты, мы свяжемся с вами!");
 }
+
+document.getElementById('feedbackForm').addEventListener('keypress', function(event) {
+    if (event.key === 'Enter' && event.target.type !== 'textarea'){
+        event.preventDefault();
+    }
+});
+
